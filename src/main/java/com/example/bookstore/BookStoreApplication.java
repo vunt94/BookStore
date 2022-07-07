bef6b2ab4d5ecccbd62cf993b2acd1026fba101d
vunt94 <vunt94.fpt@gmail.com>
2022-07-07 23:21:11 +0700

    Create folder XML

--- src/main/java/com/example/bookstore/BookStoreApplication.java
+++ src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -1,33 +1,13 @@
 package com.example.bookstore;
 
-import com.example.bookstore.model.Student;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
     public static void main(String[] args) {
 
-        try {
 
-            File file = new File("src\\main\\webapp\\WEB-INF\\xml\\book.xml");
-
-            JAXBContext jaxbContext = JAXBContext.newInstance(Student.class);
-
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
-            Student student = (Student) unmarshaller.unmarshal(file);
-
-            System.out.println(student.getFirstName());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
