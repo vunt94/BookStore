66c10b59a85cbeaccd202c77dffceb7e5494baad
vunt94 <vunt94.fpt@gmail.com>
2022-07-09 15:32:34 +0700

    Update project

--- src/main/java/com/example/bookstore/controller/CartController.java
+++ src/main/java/com/example/bookstore/controller/CartController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CartController {
 
-    @RequestMapping("/shopingCart")
+    @RequestMapping("shopingCart")
     public String shopingCart() {
         return "shopingCart";
     }
